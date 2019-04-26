0e64016c1e2542475bdc64da67b873adb031e7a8
drd-2rcl <drd.rclrd@gmail.com>
2019-04-26 16:09:02 -0300

    insert context api

--- src/App.js
+++ src/App.js
@@ -2,27 +2,50 @@ import React, { Component } from 'react';
 
 import InputFoo from './inputFoo'
 
+const ThemeContext = React.createContext('claro');
+
 class App extends Component {
   constructor() {
     super();
-    // this.handleData = this.handleData.bind(this);
     this.state = {
-      fromChild: ''
+      fromChild: '',
+      theme:  'light,'
     };
   }
-  // state = {
-  //   fromChild: ''
-  // };
+
+  changeTheme = () => {
+    this.setState({
+      theme: this.state.theme === 'light' ? 'dark' : 'light'
+    });
+  }
+
   
   handleData = (data) => {
     this.setState({
       fromChild: data
     });
   }
+
+  themedButton = (props) => {
+    return (
+      <ThemeContext.Consumer>
+        {
+          theme => (
+            <a style={{ backgroundColor: theme === 'dark' ? '#333' : '#CCC' }}>
+              Meu botão
+            </a>
+          )
+        }
+      </ThemeContext.Consumer>
+    )
+  }
   
   render() {
     return (
       <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <button onClick={this.changeTheme}>Alterar tema</button>
+        </ThemeContext.Provider>
         <InputFoo handlerFromParant={this.handleData} /> 
         <h5>Received by parent:<br />{this.state.fromChild}</h5>
       </div>
